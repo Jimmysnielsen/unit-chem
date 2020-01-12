2b012522ad901e753e3203a7c7c622b119971645
Jimmy S. Nielsen <jimmysnielsen@ginishi.dk>
2020-01-12 17:09:22 +0100

    initial commit
    initial commit 12/01/2020

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "unit-chem")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/unit-chem.scrbl" ())))
+(define pkg-desc "Data and procedures for design and calculation of chemical unit operations.")
+(define version "0.0")
+(define pkg-authors '(jimmy Sølvsteen Nielsen))
---
+++ scribblings/unit-chem.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[unit-chem
+                    racket/base]]
+
+@title{unit-chem}
+@author{jimmy Sølvsteen Nielsen}
+
+@defmodule[unit-chem]
+
+Package Description Here
